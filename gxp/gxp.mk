811212e50502a3e1af5f46279338942365890e2d
Michael Bestas <mkbestas@lineageos.org>
2024-09-07 16:30:30 +0300

    fixup! Disable userdebug diagnostics
    Change-Id: I9ec35b51e3414bded1945b7035372d91ef960414

--- gxp/gxp.mk
+++ gxp/gxp.mk
@@ -7,7 +7,9 @@ PRODUCT_PACKAGES += \
 # GXP C-API library
 PRODUCT_PACKAGES += libgxp
 # GXP Debug dump.
+ifneq (,$(filter eng, $(TARGET_BUILD_VARIANT)))
 PRODUCT_PACKAGES_DEBUG += dump_gxp
+endif
 
 BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/gxp/sepolicy
 
